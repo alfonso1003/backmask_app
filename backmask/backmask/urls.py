75e57a4d3771af150f59ed273256e414499b3516
Alfonso Hernandez <ahernandez@caltesting.org>
2017-03-29 21:46:39 -0500

    add posts view and bootstrap

--- backmask/backmask/urls.py
+++ backmask/backmask/urls.py
@@ -21,7 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),    
+    url(r'^$', views.home),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details, name="post_details"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
